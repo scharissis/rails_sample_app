2af803d5d1a9ab91ccff6273696ef17068722774
Stefano Charissis <stefano.charissis@gmail.com>
2014-01-10 17:03:54 +1100

    Update: slightly-dynamic static pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,28 +2,42 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-    describe "Home page" do
+  describe "Home page" do
 
-        it "should have the content 'Sample App'" do
-            visit '/static_pages/home'
-            expect(page).to have_content('Sample App')
-        end
-
+    it "should have the content 'TODO'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('TODO')
     end
 
-    describe "Help page" do
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
 
-        it "should have the content 'Help'" do
-            visit '/static_pages/help'
-            expect(page).to have_content('Help')
-        end
+  describe "Help page" do
+
+    it "should have the content 'TODO'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('TODO')
     end
 
-    describe "About page" do
-
-        it "should have the content 'About'" do
-            visit '/static_pages/about'
-            expect(page).to have_content('About')
-        end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
     end
-end    
+  end
+
+  describe "About page" do
+
+    it "should have the content 'TODO'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('TODO')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end
+  end
+end
