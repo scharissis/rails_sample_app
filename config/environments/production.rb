89705f057afa17970ba6bd5651b639572196ea0a
Stefano Charissis <stefano.charissis@gmail.com>
2014-01-10 15:45:02 +1100

    Update: precompile assets in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ SampleApp::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  # For heroku
+  config.assets.initialize_on_precompile = false 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
